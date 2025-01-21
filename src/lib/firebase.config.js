0a0966688ec1ee20e95696c39b79ac06ac870669
Fadhlina Wjedi <128072211+suwwui@users.noreply.github.com>
2025-01-21 14:40:19 +0800

    Update firebase.config.js
    remove api

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -7,13 +7,7 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALZ4mPsbucBli7uZod4rUHBuWZPfjX4RI",
-  authDomain: "cloudchat-11588.firebaseapp.com",
-  projectId: "cloudchat-11588",
-  storageBucket: "cloudchat-11588.firebasestorage.app",
-  messagingSenderId: "455870064718",
-  appId: "1:455870064718:web:49717c2ab667efcdbd7b1f",
-  measurementId: "G-T51RBYQ11C"
+  
 };
 
 // Initialize Firebase
